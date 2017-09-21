1085e82e3a6f4b401e35775b6473d590f0a0fd3e
osman abate <abateo@uw.edu>
2017-09-20 23:47:23 -0700

    added all functionality

--- music.js
+++ music.js
@@ -1,5 +1,6 @@
+//$('.playlst').append("<ul></ul>");
 const musicInfo = [];
-
+let $item
 function addSongFromField(event) {
   event.preventDefault();
 
@@ -23,13 +24,50 @@ function renderList() {
   $list.empty();
 
   for (const info of musicInfo) {
-    const $item = $('<li class="list-group-item">').text(info);
+    $item = info;
+    let $listItmes = $('<li class="list-group-item">').text(info);
 
-    $list.append($item)
+    $list.append($listItmes)
   }
 }
-
+//$item = musicInfo;
 $('#getPlaylistBtn').click(function (event) {
+
+  $.ajax({
+        url: `https://itunes.apple.com/search?term=${$item}&entity=song&attribute=allArtistTerm&limit=10&explicit=no`,
+        crossDomain: true,
+        dataType: 'jsonp',
+        // data: {
+        //   term: term,
+        //   entity: 'song',
+        //   limit: 24,
+        //   explicit: 'Yes'
+        // },
+        method: 'GET',
+
+        success:(data) => {
+          console.log( data);
+          data.results.map((item, index) => {
+            let firstname ="";
+            let lastname ="";
+            let artwork ="";
+            let artistIfo="";
+            console.log(item.artistName);
+            console.log(item.artworkUrl100);
+            firstname= item.artistName;
+            artwork = item.artworkUrl100;
+            artistIfo = item.collectionViewUrl;
+            $("ul").append(`<li>${firstname}</li>`);
+            $("ul").append(`<li> <a href=${artistIfo}><img src=${artwork}></a></li>`);
+
+
+          })
+
+        },
+        error: (e) => {
+          console.log(e);
+        }
+  });
   // TODO: Display a list of music.
   // You may use anything from musicInfo.
   console.log('Testing Music Call');
